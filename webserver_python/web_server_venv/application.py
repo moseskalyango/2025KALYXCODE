8d2bab77102ffa6ef7554bc464bd536256079139
moseskalyango@gmail.com <moseskalyango@gmail.com>
2025-04-06 15:29:42 -0400

    first test commit

---
+++ webserver_python/web_server_venv/application.py
@@ -0,0 +1,14 @@
+from flask import Flask 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello world. This is our new page!'
+    
+'''@app.route('/name')
+def get_name():
+    return 'Moses Kalyango are my names'
+'''
+@app.route('/contact')
+def get_contact():
+    return 'my phone number is 899231215'
